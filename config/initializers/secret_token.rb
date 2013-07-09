ba5b3e82ae684f83dac74e9e9c9eaf6dad3841c8
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-08 17:32:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amazonr::Application.config.secret_token = '910e03caf8962ec964215fc886169db730f95c83b9c475f443ffd6a56bb598cea24973d84dc2afa3055efa3aec2ba6ab4aa10c3e181a92b8154eb026032b05f0'
